f89c7e261f8be7dc96bfcd30f7d12d65ca0dfdb7
Chahel J <chaheljayaram123@gmail.com>
2025-11-09 14:49:31 +0530

    fixed logo error

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,10 @@
+import logoIcon from '../assets/chef_ai_icon.png';
+
 export default function Header(){
-    return(<div className="Header">
-            <img src="src\assets\chef_ai_icon.png" alt="icon" className="Header--icon" />
-            <span className="Header--text">Chef AI</span>
-        </div>
-    )
+    return(
+        <div className="Header">
+            <img src={logoIcon} className="Header--icon" alt="Chef AI logo" />
+            <span className="Header--text">Chef AI</span>
+        </div>
+    )
 }
